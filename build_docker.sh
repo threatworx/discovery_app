1150a76e29e1d8cfd4d6575d9c064f7e432787ff
Ketan Nilangekar <ketonne@yahoo.com>
2024-06-09 16:57:06 +0000

    Removed default cert script. Will be handled as part of setup.sh

--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,7 @@
 # Install app dependencies
 pip3 install -r /tmp/requirements.txt
 # Setup twigs update script
-printf "#!/bin/bash\n/usr/share/discovery_app/default_cert.sh\n/usr/local/bin/uwsgi --ini /opt/discovery_app/config/uwsgi.ini" > /usr/local/bin/run-app.sh
+printf "#!/bin/bash\n/usr/local/bin/uwsgi --ini /opt/discovery_app/config/uwsgi.ini" > /usr/local/bin/run-app.sh
 chmod +x /usr/local/bin/run-app.sh
 # Cleanup /tmp
 rm -f /tmp/*
