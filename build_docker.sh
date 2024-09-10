24281df9dfada05d65c21b8febce0d913daffc25
Ketan Nilangekar <ketonne@yahoo.com>
2024-09-10 17:51:38 +0000

    Install vim in image for troubleshooting

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install app dependencies
 pip3 install -r /tmp/requirements.txt
+apt update -y
+apt install vim
 # Setup twigs update script
 printf "#!/bin/bash\n/usr/sbin/service cron start\n/usr/local/bin/uwsgi --ini /opt/discovery_app/config/uwsgi.ini" > /usr/local/bin/run-app.sh
 chmod +x /usr/local/bin/run-app.sh
